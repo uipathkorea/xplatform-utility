98a039ad6d1dcb93a96880599f17075e7f796e75
Hyungsoo Kim <javaos74@gmail.com>
2019-02-14 10:40:06 +0900

    기존에 accessibility 가 설정된 경우는 무시하고 없는 경우에만 추가하도록 수정

--- add_accessibility_with_label.py
+++ add_accessibility_with_label.py
@@ -17,9 +17,10 @@ e = tree.getroot() #element instance 생성
 
 for tag in TAGS: 
     for i in e.iter( tag):
-        if 'style' in i.attrib:
-            i.attrib['style'] = i.attrib['style']  + ";accessibility:enable all '" + i.attrib['id'] + "' '';"
-        else:
+        if 'style' in i.attrib: # 다른 style 이 있는 경우 
+            if 'accessibility:enable' not in i.attrib['style']: #  accessibility가 설정되지 않은 경우에만 
+                i.attrib['style'] = i.attrib['style']  + ";accessibility:enable all '" + i.attrib['id'] + "' '';"
+        else: # style attribute가 없는 경우 
             i.attrib['style'] = "accessibility:enable all '" + i.attrib['id'] + "' '';"
 
 tree.write( 'updated_' + os.path.basename(args.file))
