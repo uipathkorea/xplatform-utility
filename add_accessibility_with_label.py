c28b52c60bb5ff67de6cf73519e254b7480b2939
Hyungsoo Kim <javaos74@gmail.com>
2019-02-14 22:00:49 +0900

    Update add_accessibility_with_label.py
    --out 옵션을 추가해서 해당 파일을 업데이트 할수 있도록 변경

--- add_accessibility_with_label.py
+++ add_accessibility_with_label.py
@@ -6,7 +6,7 @@ import argparse
 import os
 
 #accessibility label을 설정할 Tag 이름 
-TAGS = ['Edit','Radio', 'Combo', 'Button', 'Tabpage', 'Calendar']
+TAGS = ['Edit','Radio', 'Combo', 'Button', 'Tabpage', 'Calendar', 'TextArea', 'Dataset']
 
 parser = argparse.ArgumentParser(description='add accessibility style with label from element id attribute')
 parser.add_argument('--file', dest='file', help='xpltform xfdl file')
@@ -17,10 +17,14 @@ e = tree.getroot() #element instance 생성
 
 for tag in TAGS: 
     for i in e.iter( tag):
-        if 'style' in i.attrib: # 다른 style 이 있는 경우 
-            if 'accessibility:enable' not in i.attrib['style']: #  accessibility가 설정되지 않은 경우에만 
-                i.attrib['style'] = i.attrib['style']  + ";accessibility:enable all '" + i.attrib['id'] + "' '';"
-        else: # style attribute가 없는 경우 
-            i.attrib['style'] = "accessibility:enable all '" + i.attrib['id'] + "' '';"
+        if 'id' in i.attrib:
+            if 'style' in i.attrib : # 다른 style 이 있는 경우 
+                if 'accessibility:enable' not in i.attrib['style']: #  accessibility가 설정되지 않은 경우에만 
+                    i.attrib['style'] = i.attrib['style']  + ";accessibility:enable all '" + i.attrib['id'] + "' '';"
+            else : # style attribute가 없는 경우 
+                i.attrib['style'] = "accessibility:enable all '" + i.attrib['id'] + "' '';"
 
-tree.write( 'updated_' + os.path.basename(args.file))
+if args.out is not None:
+    tree.write( args.out)
+else:
+    tree.write( 'updated_' + os.path.basename(args.file))
